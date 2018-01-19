734dd75435871176ae329e4877fec4309baff140
Melvin Chu <genezonxiii@msn.com>
2018-01-19 11:01:55 +0800

    server

---
+++ MyServer.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# __author__ = '10507001'
+
+import web
+
+urls = ("/sample/(.*)", "SFSample")
+
+app = web.application(urls, globals())
+
+
+class SFSample():
+
+    def POST(self, data):
+        return """Hello World!(POST)"""
+
+    def GET(self, data):
+        return """Hello World!(GET)"""
+
+
+if __name__ == "__main__":
+    app.run()
