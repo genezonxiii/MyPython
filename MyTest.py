9b77619e614c7638755d3cbb7ab848f9944121b9
Melvin Chu <genezonxiii@msn.com>
2018-01-19 17:37:32 +0800

    change shell script/add MyTest

---
+++ MyTest.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# __author__ = '10507001'
+#! /usr/bin/env python
+
+print 'test'
+x = 1 + 3
+print x
--- runService.sh
+++ runService.sh
@@ -2,3 +2,6 @@
 echo 'start'
 python /home/jenkins/workspace/melvin0119-1@2/MyServer.py 8091 &
 echo 'end'
+
+echo 'part2'
+python MyTest.py
